3c098b3de55437c2ad192210fec1ac79570310c5
unknown <prashanthdilshaan@gmail.com>
2022-07-03 15:19:46 -0400

    fetched BoredAPI
    bored bot complete

---
+++ index.js
@@ -0,0 +1,7 @@
+btn.addEventListener("click", () => {
+    fetch("https://www.boredapi.com/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("activity").textContent = data.activity;
+        })
+}) 
